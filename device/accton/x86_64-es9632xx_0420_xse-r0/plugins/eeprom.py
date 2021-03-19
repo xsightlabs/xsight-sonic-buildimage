1158f754301deb62cc6630e0edf38ee274e9d86f
ecsonic <ecsonic@edge-core.com>
2021-03-19 15:33:50 +0000

    [es9632xx] Add new platform
    Signed-off-by: ecsonic <ecsonic@edge-core.com>

---
+++ device/accton/x86_64-es9632xx_0420_xse-r0/plugins/eeprom.py
@@ -0,0 +1,23 @@
+try:
+    import binascii
+    import time
+    import optparse
+    import warnings
+    import os
+    import sys
+    from sonic_eeprom import eeprom_base
+    from sonic_eeprom import eeprom_tlvinfo
+    import subprocess
+except ImportError as e:
+    raise ImportError(str(e) + "- required module not found")
+
+
+class board(eeprom_tlvinfo.TlvInfoDecoder):
+    _TLV_INFO_MAX_LEN = 256
+
+    def __init__(self, name, path, cpld_root, ro):
+        self.eeprom_path = "/sys/bus/i2c/devices/23-0055/eeprom"
+        # Two i2c buses might get flipped order, check them both.
+        if not os.path.exists(self.eeprom_path):
+            self.eeprom_path = "/sys/bus/i2c/devices/23-0055/eeprom"
+        super(board, self).__init__(self.eeprom_path, 0, '', True)
---
+++ platform/broadcom/sonic-platform-modules-accton/es9632xx-0420-xse/utils/es9632xx_0420_xse_monitor.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2017 Accton Technology Corporation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# ------------------------------------------------------------------
+# HISTORY:
+#    mm/dd/yyyy (A.D.)
+#    11/13/2017: Polly Hsu, Create
+#    1/10/2018: Jostar modify for as7716_32
+#    2/27/2018: Roy Lee modify for as7312_54x
+# ------------------------------------------------------------------
+
+try:
+    import getopt
+    import sys
+    import logging
+    import logging.config
+    import time  # this is only being used as part of the example
+    import signal
+    from es9632xx_0420_xse.fanutil import FanUtil
+    from es9632xx_0420_xse.thermalutil import ThermalUtil
+except ImportError as e:
+    raise ImportError('%s - required module not found' % str(e))
+
+# Deafults
+VERSION = '1.0'
+FUNCTION_NAME = 'es9632xx_0420_xse_monitor'
+DUTY_MAX = 100
+
+global log_file
+global log_level
+
+#   (LM75_1+ LM75_2+ LM75_3) is LM75 at i2c addresses 0x48, 0x49, and 0x4A.
+#   TMP = (LM75_1+ LM75_2+ LM75_3)/3
+#1. If TMP < 35, All fans run with duty 31.25%.
+#2. If TMP>=35 or the temperature of any one of fan is higher than 40,
+#   All fans run with duty 50%
+#3. If TMP >= 40 or the temperature of any one of fan is higher than 45,
+#   All fans run with duty 62.5%.
+#4. If TMP >= 45 or the temperature of any one of fan is higher than 50,
+#   All fans run with duty 100%.
+#5. Any one of 6 fans is fault, set duty = 100%.
+#6. Direction factor. If it is B2F direction, duty + 12%.
+
+ # MISC:
+ # 1.Check single LM75 before applied average.
+ # 2.If no matched fan speed is found from the policy,
+ #     use FAN_DUTY_CYCLE_MIN as default speed
+ # Get current temperature
+ # 4.Decision 3: Decide new fan speed depend on fan direction/current fan speed/temperature
+
+
+
+
+# Make a class we can use to capture stdout and sterr in the log
+def handler(signum, frame):
+    fan = FanUtil()
+    logging.debug('INFO:Cause signal %d, set fan speed max.', signum)
+    fan.set_fan_duty_cycle(DUTY_MAX)
+    sys.exit(0)
+
+def main(argv):
+    log_file = '%s.log' % FUNCTION_NAME
+    log_level = logging.INFO
+    if len(sys.argv) != 1:
+        try:
+            opts, args = getopt.getopt(argv,'hdl:',['lfile='])
+        except getopt.GetoptError:
+            print 'Usage: %s [-d] [-l <log_file>]' % sys.argv[0]
+            return 0
+        for opt, arg in opts:
+            if opt == '-h':
+                print 'Usage: %s [-d] [-l <log_file>]' % sys.argv[0]
+                return 0
+            elif opt in ('-d', '--debug'):
+                log_level = logging.DEBUG
+            elif opt in ('-l', '--lfile'):
+                log_file = arg
+
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
+    #monitor = es9632xx_monitor(log_file, log_level)
+
+    # Loop forever, doing something useful hopefully:
+    while True:
+        #monitor.manage_fans()
+        time.sleep(10)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
