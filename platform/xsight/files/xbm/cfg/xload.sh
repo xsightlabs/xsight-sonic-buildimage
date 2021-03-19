d678007da76a840251248fcdaac35ab6c999598f
Borys Barkauskas <bbarkauskas@larch-networks.net>
2021-03-19 21:28:12 +0200

    set netdev_mode=1 when insmod-ing xpci.ko

--- platform/xsight/files/xbm/cfg/xload.sh
+++ platform/xsight/files/xbm/cfg/xload.sh
@@ -3,7 +3,7 @@
 # 3 - Info
 # 4 - Debug
 # 5 - Debug with Packet trace
-sudo insmod /home/admin/xbm/cfg/xpci.ko attach_if="xcpu" num_of_ports=16 debug_level=4
+sudo insmod /home/admin/xbm/cfg/xpci.ko attach_if="xcpu" num_of_ports=16 debug_level=4 netdev_mode=1
 
 echo "echo 4 > /sys/module/xpci/parameters/debug_level"
 echo 4 > /sys/module/xpci/parameters/debug_level
