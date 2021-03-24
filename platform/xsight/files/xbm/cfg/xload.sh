480cde9f784fa07cc2fb8647e67cd50e42289b7a
Borys Barkauskas <bbarkauskas@larch-networks.net>
2021-03-24 23:26:20 +0200

    decrease xpci module debug verbosity to 3

--- platform/xsight/files/xbm/cfg/xload.sh
+++ platform/xsight/files/xbm/cfg/xload.sh
@@ -3,7 +3,7 @@
 # 3 - Info
 # 4 - Debug
 # 5 - Debug with Packet trace
-sudo insmod /home/admin/xbm/cfg/xpci.ko attach_if="xcpu" num_of_ports=16 debug_level=4 netdev_mode=1
+sudo insmod /home/admin/xbm/cfg/xpci.ko attach_if="xcpu" num_of_ports=16 debug_level=3 netdev_mode=1
 
-echo "echo 4 > /sys/module/xpci/parameters/debug_level"
-echo 4 > /sys/module/xpci/parameters/debug_level
+echo "echo 3 > /sys/module/xpci/parameters/debug_level"
+echo 3 > /sys/module/xpci/parameters/debug_level
