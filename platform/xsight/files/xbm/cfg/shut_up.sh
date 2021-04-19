12273f36137e82488ddbc520948e2a02d150b94c
Borys Barkauskas <bbarkauskas@larch-networks.net>
2021-04-19 15:49:12 +0000

    remove unused xbm scripts

--- platform/xsight/files/xbm/cfg/shut_up.sh
+++
@@ -1,4 +0,0 @@
-./setup_host.sh Ethernet1 192.168.1.1/24
-./setup_host.sh Ethernet2 192.168.2.1/24
-./setup_host.sh Ethernet3 192.168.3.1/24
-./setup_host.sh Ethernet4 192.168.4.1/24
--- platform/xsight/files/xbm/cfg/start.sh
+++ platform/xsight/files/xbm/cfg/start.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-#echo ">>> Configuring namespaces"
-#read -p "--- Press enter to continue ---"
-#/home/admin/xbm/cfg/ns_netdev.sh
-
-#echo ">>> Starting VETH pairs"
-#/home/admin/xbm/cfg/up_veth_ifs.sh
-#sleep 3
-
 echo ">>> Start XBM"
 #read -p "--- Press enter to continue ---"
 #read -p '---- Please run XBM ---'
 rm -f /home/admin/xbm/log/*
 /home/admin/xbm/cfg/run_xbm.sh&
-#sleep 30
 
 echo ">>> Sleeping 10"
 sleep 10
@@ -24,21 +15,3 @@ echo ">>> Routes"
 ip ro
 echo ">>> Neighbors"
 ip neigh
-
-#echo ">>> Down all interfaces"
-#read -p "--- Press enter to continue ---"
-#/home/admin/xbm/cfg/down_netdev.sh
-
-#echo ">>> Start SwitchLink"
-#source xs_sai_env.sh
-#read -p "--- Press enter to continue ---"
-#../bin/xswlink --sai-only&
-
-#read -p "--- Press enter till SwitchLink is READY ---"
-
-#echo ">>> Init interfaces"
-#/home/admin/xbm/cfg/init_rtnl.sh
-
-#read -p "--- Press enter to continue ---"
-#/home/admin/xbm/cfg/cfg_netdev.sh
-
