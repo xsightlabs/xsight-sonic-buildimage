696a2cebbcb0e13847414ba8f59596759e1583a3
Borys Barkauskas <bbarkauskas@larch-networks.net>
2021-04-19 15:58:24 +0000

    rework start-up scripts

--- files/scripts/syncd.sh
+++ files/scripts/syncd.sh
@@ -82,7 +82,6 @@ function startplatform() {
     fi
 
     if [[ x"$sonic_asic_platform" == x"xsight" ]]; then
-        debug "Starting XBM..."
         /home/admin/x1/start.sh
     fi
 
--- platform/xsight/files/x1/start.sh
+++ platform/xsight/files/x1/start.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
-PORT_NUM=16
+USE_XBM=true
 ATTACH_IF="xcpu"
+PORT_NUM=16
 DEBUG_LEVEL=3
 NETDEV_MODE=1
 ONIE_MACHINE=`sed -n -e 's/^.*onie_machine=//p' /host/machine.conf`
+CFG_FILE="/home/admin/x1/hw.cfg"
 
-echo ">>> Configuring CPU port VETH devices"
+function get_hw_params {
+    if [[ ! -f ${CFG_FILE} ]]; then
+        echo ">>> ERROR! Config file not found: ${CFG_FILE}"
+        exit 1
+    fi
 
-# Setup CPU port
-ip link add name veth0 type veth peer name xcpu
-ip link set dev veth0 up
-ip link set dev xcpu up
+    SYS_MODE=`sed -n -e 's/^.*sys_mode[[:blank:]]*=[[:blank:]]*//p' ${CFG_FILE}`
+    XPCI_NETDEV=`sed -n -e 's/^.*xpci_netdev[[:blank:]]*=[[:blank:]]*//p' ${CFG_FILE}`
+}
+
+#remove use_xbm flag file before examine simulation mode
+rm -f /etc/sonic/use_xbm
+
+if [[ ${ONIE_MACHINE} == *"kvm"* ]]; then
+    echo ">>> Configuring CPU port VETH devices"
+    # Setup CPU port
+    ip link add name veth0 type veth peer name xcpu
+    ip link set dev veth0 up
+    ip link set dev xcpu up
+else
+    get_hw_params
+    if [[ ${SYS_MODE} == "asic" ]]; then
+        USE_XBM=false;
+        NETDEV_MODE=0
+    fi
+
+    if [[ ! -d /sys/class/net/${XPCI_NETDEV} ]]; then
+        echo ">>> ERROR! No such interface: ${XPCI_NETDEV}"
+        exit 1
+    fi
+    ATTACH_IF=${XPCI_NETDEV}
+fi
 
 echo ">>> Re-load NetDev"
 #read -p "--- Press enter to continue ---"
@@ -23,7 +51,7 @@ rmmod xpci
 #               3 - Info
 #               4 - Debug
 #               5 - Debug with Packet trace
-insmod /home/admin/xbm/cfg/xpci.ko attach_if=${ATTACH_IF} num_of_ports=${PORT_NUM} debug_level=${DEBUG_LEVEL} netdev_mode=${NETDEV_MODE}
+insmod /home/admin/x1/xpci.ko attach_if=${ATTACH_IF} num_of_ports=${PORT_NUM} debug_level=${DEBUG_LEVEL} netdev_mode=${NETDEV_MODE}
 
 echo ">>> Sleeping 5"
 sleep 5
@@ -31,7 +59,7 @@ sleep 5
 #echo ">>> Set XPCI debug level to INFO(3)"
 #echo ${DEBUG_LEVEL} > /proc/sys/dev/xpci_dev/debug_level
 
-if [[ ${ONIE_MACHINE} == *"kvm"* ]]; then
+if [[ ${USE_XBM} == "true" ]]; then
     echo ">>> Simulation mode. Use xbm!"
     touch /etc/sonic/use_xbm
     /home/admin/xbm/cfg/start.sh
