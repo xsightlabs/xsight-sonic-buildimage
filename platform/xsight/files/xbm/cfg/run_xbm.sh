1572938ba42d20451cd0ae584c61854eb8204ddf
Roman Zhurakivskyy <rzhurak@larch-networks.com>
2021-12-24 12:31:06 +0000

    XBM log level set to off
    Unfortunately even error log level produces over 60000 messages in 1-2 minutes

--- platform/xsight/files/xbm/cfg/run_xbm.sh
+++ platform/xsight/files/xbm/cfg/run_xbm.sh
@@ -27,7 +27,7 @@ do
 done
 
 /home/admin/xbm/bin/xbm --device-id 0 --name standalone0 --thrift-port 49153 \
-        --log-file /home/admin/xbm/log/standalone0_log_file.log -L error --log-flush \
+        --log-file /home/admin/xbm/log/standalone0_log_file.log -L off --log-flush \
         --enforce-validation --dpu-x-size 16 --dpu-y-size 16 --ifu-config-mode 0 \
         -i 120@eth1 -i 122@eth2 -i 124@eth3 -i 126@eth4 -i 248@eth5 -i 250@eth6 -i 252@eth7 -i 254@eth8 \
         -i 104@eth9 -i 106@eth10 -i 108@eth11 -i 110@eth12 -i 232@eth13 -i 234@eth14 -i 236@eth15 -i 238@eth16 \
