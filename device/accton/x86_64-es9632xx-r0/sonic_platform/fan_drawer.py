517085d39bfba34838ef0ad0a72e7bcbb6b400d0
Yuri Danilov <yurid@xsightlabs.com>
2022-01-06 16:56:59 +0200

    SON-754: added set_status_led API in fan_drawer

--- device/accton/x86_64-es9632xx-r0/sonic_platform/fan_drawer.py
+++ device/accton/x86_64-es9632xx-r0/sonic_platform/fan_drawer.py
@@ -10,11 +10,12 @@
 try:
     from sonic_platform_base.fan_drawer_base import FanDrawerBase
     from sonic_platform.fan import Fan
+    from .helper import APIHelper
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
 FANS_PER_FANTRAY = 2
-
+FAN_LED_FILE = "/sys/class/leds/es9632_led::fan/brightness"
 
 class FanDrawer(FanDrawerBase):
     """Platform-specific Fan class"""
@@ -23,7 +24,9 @@ class FanDrawer(FanDrawerBase):
 
         FanDrawerBase.__init__(self)
         # FanTray is 0-based in platforms
+        self._api_helper=APIHelper()
         self.fantrayindex = fantray_index
+        self.status_led_state = None
         for i in range(FANS_PER_FANTRAY):
             self._fan_list.append(Fan(fantray_index, i))
 
@@ -96,4 +99,28 @@ class FanDrawer(FanDrawerBase):
             integer: The 1-based relative physical position in parent device
         """
         return (self.fantrayindex + 1)
+
+    def set_status_led(self, color):
+        """
+        Sets the state of the fan drawer status LED
+        Args:
+            color: A string representing the color with which to set the
+                   fan drawer status LED
+        Returns:
+            bool: True if status LED state is set successfully, False if not
+        """
+        self.status_led_state = color
+        set_status = 0
+        if "off" == self.status_led_state:
+            set_status = 0
+        elif "amber" == self.status_led_state:
+            set_status = 3
+        elif "red" == self.status_led_state:
+            set_status = 3
+        elif "green" == self.status_led_state:
+            set_status = 1
+        else:
+            return False
+        self._api_helper.write_txt_file(FAN_LED_FILE, set_status)
+        return True
 
