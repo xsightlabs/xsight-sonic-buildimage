2302f7fc50865727c92075a14a3dc1e4f6c39a81
Roman Zhurakivskyy <rzhurak@larch-networks.com>
2022-01-24 08:05:20 +0000

    SON-789 move to xplt 2.1.1

--- platform/xsight/xplt.mk
+++ platform/xsight/xplt.mk
@@ -3,6 +3,8 @@
 # TODO: bring and install the deb per box type.
 
 # Add platform specific tools
-XPLT_TOOLS = xplt_es9632xq_sonicnos-2.1.1-0-g4f5e42c_amd64.deb
-$(XPLT_TOOLS)_URL = http://x-nexus:8081/repository/xplt-es9632x/deb/$(XPLT_TOOLS)
-SONIC_ONLINE_DEBS += $(XPLT_TOOLS)
+XPLT_VERSION=2.1.1-0-g4f5e42c
+XPLT_TOOLS = xplt_es9632xq_sonicnos-$(XPLT_VERSION)_amd64.deb
+XPLT_TOOLS_URL = http://x-nexus:8081/repository/xplt-es9632x/deb/$(XPLT_TOOLS)
+
+export XPLT_TOOLS_URL
