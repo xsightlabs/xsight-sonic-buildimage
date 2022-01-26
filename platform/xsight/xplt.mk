0aad0827522a77b0a6671d4e555c539b0761e963
Michael Zaidman <michaelz@xsightlabs.com>
2022-01-26 13:57:37 +0200

    SON-833: xplt: update xplt to 2.1.1-4 version
    Signed-off-by: Michael Zaidman <michaelz@xsightlabs.com>

--- platform/xsight/xplt.mk
+++ platform/xsight/xplt.mk
@@ -3,7 +3,7 @@
 # TODO: bring and install the deb per box type.
 
 # Add platform specific tools
-XPLT_VERSION=2.1.1-0-g4f5e42c
+XPLT_VERSION=2.1.1-4-g537541d
 XPLT_TOOLS = xplt_es9632xq_sonicnos-$(XPLT_VERSION)_amd64.deb
 XPLT_TOOLS_URL = http://x-nexus:8081/repository/xplt-es9632x/deb/$(XPLT_TOOLS)
 
