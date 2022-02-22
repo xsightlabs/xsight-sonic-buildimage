710acf7b0c0e08c720960fe212d7cd81162482ec
Roman Zhurakivskyy <rzhurak@larch-networks.com>
2022-02-22 11:44:18 +0000

    SON-235: add comment for xplt location

--- platform/xsight/xplt.mk
+++ platform/xsight/xplt.mk
@@ -5,6 +5,7 @@
 # Add platform specific tools
 XPLT_VERSION=2.1.1-4-g537541d
 XPLT_TOOLS = xplt_es9632xq_sonicnos-$(XPLT_VERSION)_amd64.deb
+# deb is located on x-nexus
 XPLT_TOOLS_URL = http://172.20.4.62:8081/repository/xplt-es9632x/deb/$(XPLT_TOOLS)
 
 export XPLT_TOOLS_URL
