3f8c7d63d5d19ca51df3bd3bda21dfeb3017d204
Borys Barkauskas <bbarkauskas@larch-networks.net>
2022-05-18 15:17:55 +0000

    SON-1112: docker syncd version added plus dependency for dbg

--- platform/xsight/docker-syncd-xsight.mk
+++ platform/xsight/docker-syncd-xsight.mk
@@ -7,3 +7,13 @@ $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(LIBTHRIFT)
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += --env ASAN_SAI_PLUGIN=${ASAN_SAI_PLUGIN} # used in address-sanitizer sai-plugin check
 
+$(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
+                                $(LIBSWSSCOMMON_DBG) \
+                                $(LIBSAIMETADATA_DBG) \
+                                $(LIBSAIREDIS_DBG)
+
+$(DOCKER_SYNCD_BASE)_VERSION = 1.0.0
+$(DOCKER_SYNCD_BASE)_PACKAGE_NAME = syncd
+
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
+
