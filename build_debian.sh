d672b0e1434b02d629e6b68ac778890f4c446b8c
Borys Barkauskas <bbarkauskas@larch-networks.net>
2022-08-16 22:52:16 +0000

    SON-1350: switch back to sonic vanilla ixgbe driver

--- build_debian.sh
+++ build_debian.sh
@@ -248,13 +248,6 @@ fi
 ## Add mtd and uboot firmware tools package
 sudo LANG=C chroot $FILESYSTEM_ROOT apt-get -y install u-boot-tools libubootenv-tool mtd-utils device-tree-compiler
 
-if [[ $CONFIGURED_ARCH == amd64 ]]; then
-    ## Remove default intel ixgbe driver
-    if [ -f $FILESYSTEM_ROOT/lib/modules/${LINUX_KERNEL_VERSION}-amd64/kernel/drivers/net/ethernet/intel/ixgbe/ixgbe.ko ]; then
-        sudo rm -r $FILESYSTEM_ROOT/lib/modules/${LINUX_KERNEL_VERSION}-amd64/kernel/drivers/net/ethernet/intel/ixgbe
-    fi
-fi
-
 ## Install docker
 echo '[INFO] Install docker'
 ## Install apparmor utils since they're missing and apparmor is enabled in the kernel
