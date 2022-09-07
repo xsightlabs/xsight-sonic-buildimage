89a7f784821d81cceff1af3e8e98bfec7f0655b0
Anton Parkhomenko <aparkhomenko@larch-networks.com>
2022-09-07 10:50:05 +0000

    SON-1392: fix xsight-sai.mk and xpci.mk
    fixes:
    
    * use XSIGHT_URL_PREFIX instead of XSIGHT_URL to prevent
        theating it as internal SONiCs variable
    
    * use different URL_PREFIXes for xsight-sai.mk ad xpci.mk
        It allows to have different URL_PREFIXes for this modules.
        Using same variable name somehow leads to overwring this
        variange in xsight-sai.mk with value from xpci.mk
    
    * wrap _URLs variable with double qoutes as alternative
        to exporting
    
    * _URLs must be redefined after add_derived_package(), because
        add_derived_package() inherits _URL from parent
    
    * no need to add SONIC_ONLINE_DEBS for derived packages
    
    * fix XBM target, add_derived_package() requires two
        arguments
    
    * remove $(XSIGHT_LIBSAI_DEV)_DEPENDS += $(XSIGHT_LIBSAI)
        a few vendors have this line, but add_derived_package()
        includes this assignment

--- platform/xsight/xpci.mk
+++ platform/xsight/xpci.mk
@@ -4,18 +4,13 @@ XPCI_VERSION = 1.0
 
 export XPCI_VERSION
 
-XSIGHT_URL = file:///sonic
+XSIGHT_XPCI_URL_PREFIX = "file:///sonic"
 XPCI = xpci-dkms_$(XPCI_VERSION)_amd64.deb
-$(XPCI)_URL = $(XSIGHT_URL)/$(XPCI)
+$(XPCI)_URL = "$(XSIGHT_XPCI_URL_PREFIX)/$(XPCI)"
 SONIC_ONLINE_DEBS += $(XPCI)
-
-$(eval $(call add_derived_package,$(XPCI)))
 
 KERNEL_XPCI = xpci-modules-$(KVERSION)_$(XPCI_VERSION)_amd64.deb
 $(KERNEL_XPCI)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON) $(XPCI)
 
 $(KERNEL_XPCI)_SRC_PATH = $(PLATFORM_PATH)/xpci
 SONIC_MAKE_DEBS += $(KERNEL_XPCI)
-
-$(eval $(call add_derived_package,$(KERNEL_XPCI)))
-
