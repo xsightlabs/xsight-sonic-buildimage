f4869d50e1e332f84f1422eda941cca14d5030ef
Michael Zaidman <michaelz@xsightlabs.com>
2022-09-19 22:59:16 +0300

    SON-1404: update xplt deb package
    Signed-off-by: Michael Zaidman <michaelz@xsightlabs.com>

--- platform/xsight/xplt.mk
+++ platform/xsight/xplt.mk
@@ -3,7 +3,7 @@
 # TODO: bring and install the deb per box type.
 
 # Add platform specific tools
-XPLT_VERSION=2.2.0-10-g01efad4
+XPLT_VERSION=3.0.0-1-g31a6f59
 
 XPLT_TOOLS = xplt_es9632xq_sonicnos-$(XPLT_VERSION)_amd64.deb
 $(XPLT_TOOLS)_URL = "http://172.20.4.62:8081/repository/xplt-es9632x/deb/"$(XPLT_TOOLS)
