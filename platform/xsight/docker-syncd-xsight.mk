5fc255f81c31c37427e53e4b6a8390c0a369d98c
Anton Parkhomenko <aparkhomenko@larch-networks.com>
2023-08-17 15:07:10 +0000

    SON-1626: use Thrift 14 syncd with SAITHRIFT_V2=y

--- platform/xsight/docker-syncd-xsight.mk
+++ platform/xsight/docker-syncd-xsight.mk
@@ -4,9 +4,6 @@ DOCKER_SYNCD_PLATFORM_CODE = xsight
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(LIBTHRIFT)
-ifeq ($(XBM_EXISTS),y)
-$(DOCKER_SYNCD_BASE)_DEPENDS += $(XSIGHT_XBM)
-endif
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += --env ASAN_SAI_PLUGIN=${ASAN_SAI_PLUGIN} # used in address-sanitizer sai-plugin check
 
