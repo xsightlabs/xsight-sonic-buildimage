4bb32ac028284b2acdc4879820e44926575846f1
Anton Parkhomenko <aparkhomenko@larch-networks.com>
2023-08-17 15:15:48 +0000

    SON-1661: use Debian Bullseye instead of Buster for syncd container

--- platform/xsight/docker-syncd-xsight.mk
+++ platform/xsight/docker-syncd-xsight.mk
@@ -1,7 +1,7 @@
 # docker image for XSight Labs syncd
 
 DOCKER_SYNCD_PLATFORM_CODE = xsight
-include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+include $(PLATFORM_PATH)/../template/docker-syncd-bullseye.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(LIBTHRIFT)
 
