d0a64b8d6689e5417379138a2ed60dfad4b786e5
Asher Oved <ashero@xsightlabs.com>
2024-10-08 23:19:33 +0300

    XSW-8688: x2evb: use power_override for xcvrs lpm mode control
    Use power_override for xcvrs lpm mode control in sonic_platform package
    Use power_override for xcvrs lpm mode control in sfputil.py plugin
    
    Set lpm power_override to low power and the lpm discrete pin to high power
    during sfp module instantiation, resulting in low power mode.
    This is done to address an existing problem in present/lpmode discrete pins.
    The present pin indicates correctly only when the lpmode pin is in high power,
    otherwise, the present pin indicates present state also to not connected xcvrs.
    
    Set xcvr power_override to low power at each port insertion
    
    Signed-off-by: Asher Oved <ashero@xsightlabs.com>

--- device/xsight/x86_64-xsight_x2evb-r0/plugins/sfputil.py
+++ device/xsight/x86_64-xsight_x2evb-r0/plugins/sfputil.py
@@ -12,6 +12,9 @@ try:
 except ImportError as e:
     raise ImportError("%s - required module not found" % str(e))
 
+CMIS_REG_MODULE_GLOBAL_CONTROL = 26
+CMIS_REG_MODULE_STATE = 3
+
 SFP_STATUS_INSERTED = '1'
 SFP_STATUS_REMOVED = '0'
 
@@ -89,23 +92,40 @@ class SfpUtil(SfpUtilBase):
              return False
 
     def get_low_power_mode(self, port_num):
-        if port_num < self.port_start or port_num > self.port_end:
+        """For more information see 'get_lpmode' in chassis.py at sonic_platform package"""
+        if port_num < self.PORT_START or port_num > self.PORT_END:
             return False
-
-        port_bit = 1 << (port_num - 1)
-        val = self.xcvrpins.get_xcvr_lowpower_pins()
-        if val is not None:
-            return (val & port_bit) == port_bit
-        else:
+        if not self.get_presence():
+            return False
+        try:
+            with open(self.port_to_eeprom_mapping[port_num], "rb") as fd:
+                fd.seek(CMIS_REG_MODULE_STATE)
+                lpmode = ord(fd.read(1))
+                # Low Power Mode if "Module state" bits is "ModuleLowPwr", High Power Mode otherwise.
+                if ((lpmode & 0xE) == 0x2):
+                    return True
+                else:
+                    return False
+        except Exception as e:
+            print("Error: unable to open file: {}".format(str(e)))
             return False
 
     def set_low_power_mode(self, port_num, lpmode):
+        """For more information see 'set_power_override' in chassis.py at sonic_platform package"""
         if port_num < self.PORT_START or port_num > self.PORT_END:
             return False
-
-        val = '1' if lpmode is True else '0'
-        port_bit = 1 << (port_num - 1)
-        self.xcvrpins.set_xcvr_lowpower_pins(port_bit, val)
+        if not self.get_presence():
+            return False
+        try:
+            buffer = create_string_buffer(1)
+            buffer[0] = (1 << 4) if lpmode else 0
+            with open(self.port_to_eeprom_mapping[port_num], "r+b") as fd:
+                fd.seek(CMIS_REG_MODULE_GLOBAL_CONTROL)
+                fd.write(buffer[0])
+                time.sleep(0.01)
+        except Exception as e:
+            print("Error: unable to open file: {}".format(str(e)))
+            return False
         return True
 
     def reset(self, port_num):
