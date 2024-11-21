c295bf57d12c383134441a76e585ddec2c249c70
Asher Oved <ashero@xsightlabs.com>
2024-11-21 14:32:28 +0200

    XSW-8607: x2: optoe: change write timeout to 150 ms to keep responsiveness
    Change write timeout to 150[ms] to maintain responsiveness if the transceiver
    is disconnected.
    
    Signed-off-by: Asher Oved <ashero@xsightlabs.com>

--- platform/xsight/sonic-platform-modules-accton/es9618xx/utils/es9618xx_util.py
+++ platform/xsight/sonic-platform-modules-accton/es9618xx/utils/es9618xx_util.py
@@ -305,7 +305,7 @@ def device_install():
                 return status
 
         (status, output) = \
-            log_os_system('echo 500' + ' > /sys/bus/i2c/devices/'
+            log_os_system('echo 150' + ' > /sys/bus/i2c/devices/'
                             + str(sfp_map[i]) + '-0050/write_timeout', 1)
         if status:
             print(output)
