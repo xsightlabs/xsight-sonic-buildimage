b82a64f472d928e6c4216fd7f7aa1d6711e1f77c
Asher Oved <ashero@xsightlabs.com>
2025-04-08 15:19:08 +0300

    SON-2130: es9618xx: xcvrs: config xcvrs in standalone mode
    Update invocation of `config.py` to run in standalone mode
    
    Signed-off-by: Asher Oved <ashero@xsightlabs.com>

--- platform/xsight/files/xlx/start.sh
+++ platform/xsight/files/xlx/start.sh
@@ -187,7 +187,7 @@ if [ ! -f /tmp/xbooted ]; then
     if [ ${SYS_MODE,,} != "xbm" ]; then
         if [ -d $XPLT_UTL ]; then
             echo ">>> Configure xcvrs"
-            cd $XPLT_UTL/xcvrs && /opt/xplt/venv/bin/python config.py -m 1
+            cd $XPLT_UTL/xcvrs && /opt/xplt/venv/bin/python config.py -s -m 1
         else
             echo "ERROR: No $XPLT_UTL found!"
         fi
