765497e4af2ed794430396009ad85c4e986dd3e3
Michael Zaidman <michaelz@xsightlabs.com>
2025-06-29 12:54:30 +0000

    [ES9618XX] Use platform.json for ASIC sensor polling configuration
    With the enhancement introduced in PR #20826 -
    "[asic_sensors] Generate the ASIC sensor polling configuration
    based on the platform.json" - we can now define ASIC sensor polling
    settings directly in platform.json.
    
    This change removes the need for a custom.json override and simplifies
    platform-specific configuration by centralizing the polling interval
    and status settings under platform.json.
    
    Signed-off-by: Michael Zaidman <michaelz@xsightlabs.com>

--- platform/xsight/files/xlx/start.sh
+++ platform/xsight/files/xlx/start.sh
@@ -20,23 +20,6 @@ XSIGHT_PCI_ID=""
 XSIGHT_DEVICE=""
 FIRSTBOOT="/tmp/notify_firstboot_to_platform"
 
-# Adding the custom.json content at run time is a quick workaround, which we do in our code.
-# It would be more appropriate to do it at build time, but it requires modifying the system-wide build script.
-# Please note that the following solution may not work if the system boot timing is changed in the future and
-# the /tmp/notify_firstboot_to_platform file is removed before the start.sh is invoked.
-# The current solution is used temporarily and the issue could also be resolved in the following ways:
-# 1. By adding the custom settings in build time through JSON template file (require change of system-wide files)
-# 2. Fixing incorrect removing of FIRSTBOOT indicator file (require a change of system-wide files)
-if [ -f $FIRSTBOOT ]; then
-    PLATFORM=$(sed -n 's/onie_platform=\(.*\)/\1/p' /host/machine.conf)
-
-    # update default config from custom.json
-    if [ -f /usr/share/sonic/device/$PLATFORM/custom.json ]; then
-        sonic-cfggen --from-db -j /usr/share/sonic/device/$PLATFORM/custom.json --print-data > /etc/sonic/config_db.json
-        sonic-cfggen -j /usr/share/sonic/device/$PLATFORM/custom.json --write-to-db
-    fi
-fi
-
 fname=$(basename $0)
 
 # The eth0 interface intermittently disappears on reboot.
